fb6475e8f9639b7ea6c9e9a430ebace3e708577f
hihihaha <hieutedam2k2@gmail.com>
2024-01-21 14:03:11 +0700

    :muscle: create line chart, switch light and fan

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,203 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import { LineChart } from "@mui/x-charts";
+import { Box, Stack } from "@mui/system";
+import ThermostatOutlinedIcon from "@mui/icons-material/ThermostatOutlined";
+import WaterDropOutlinedIcon from "@mui/icons-material/WaterDropOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
+import FilterVintageOutlinedIcon from "@mui/icons-material/FilterVintageOutlined";
+import { FormControlLabel, Switch, Typography } from "@mui/material";
+
+const s = io("http://localhost:4000/");
+
+interface DataFromServer {
+  labels: number[];
+  values: number[];
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<DataFromServer>({ labels: [], values: [] });
+
+  useEffect(() => {
+    s.on("dataUpdate", (newData: any) => {
+      setData((prev) => {
+        const checkData = {
+          labels: [...prev.labels, newData.label],
+          values: [...prev.values, newData.value],
+        };
+        const updatedData = checkData;
+        // console.log(updatedData);
+
+        if (checkData.labels.length > 6) {
+          updatedData.labels.shift();
+          updatedData.values.shift();
+        }
+        // console.log(updatedData);
+
+        return updatedData;
+      });
+    });
+    return () => {
+      // Cleanup on component unmount
+      s.off("dataUpdate");
+    };
+  }, []);
+
+  // console.log(data);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Stack direction={"row"} spacing={2} marginTop={10}>
+        <Box justifyContent={"center"}>
+          <Typography variant="h5" component="h5">
+            <ThermostatOutlinedIcon
+              sx={{ verticalAlign: "middle" }}
+              color={"error"}
+            />
+            Nhiệt độ
+          </Typography>
+          <LineChart
+            xAxis={[
+              {
+                scaleType: "point",
+                data: data.labels,
+                label: "Thời gian",
+              },
+            ]}
+            yAxis={[
+              {
+                id: "a",
+                scaleType: "linear",
+                min: 0,
+                max: 120,
+                label: "Nhiệt độ",
+              },
+            ]}
+            series={[
+              {
+                color: "#ff0000",
+                yAxisKey: "a",
+                data: data.values,
+                // area: true,
+                curve: "catmullRom",
+              },
+            ]}
+            width={400}
+            height={300}
+          />
+        </Box>
+        <Box justifyContent={"center"}>
+          <Typography variant="h5" component="h5">
+            <WaterDropOutlinedIcon
+              sx={{ verticalAlign: "middle" }}
+              color={"primary"}
+            />
+            Độ ẩm
+          </Typography>
+          <LineChart
+            xAxis={[
+              {
+                scaleType: "point",
+                data: data.labels,
+                label: "Thời gian",
+              },
+            ]}
+            yAxis={[
+              {
+                id: "a",
+                scaleType: "linear",
+                min: 0,
+                max: 120,
+                label: "Nhiệt độ",
+              },
+            ]}
+            series={[
+              {
+                color: "blue",
+                yAxisKey: "a",
+                data: data.values,
+                // area: true,
+                curve: "catmullRom",
+              },
+            ]}
+            width={400}
+            height={300}
+          />
+        </Box>
+        <Box justifyContent={"center"}>
+          <Typography variant="h5" component="h5">
+            <LightModeOutlinedIcon
+              sx={{ verticalAlign: "middle" }}
+              color="warning"
+            />
+            Ánh sáng
+          </Typography>
+          <LineChart
+            xAxis={[
+              {
+                scaleType: "point",
+                data: data.labels,
+                label: "Thời gian",
+              },
+            ]}
+            yAxis={[
+              {
+                id: "a",
+                scaleType: "linear",
+                min: 0,
+                max: 120,
+                label: "Nhiệt độ",
+              },
+            ]}
+            series={[
+              {
+                color: "yellow",
+                yAxisKey: "a",
+                data: data.values,
+                // area: true,
+                curve: "catmullRom",
+              },
+            ]}
+            width={400}
+            height={300}
+          />
+        </Box>
+      </Stack>
+
+      <Stack
+        direction={"row"}
+        spacing={2}
+        justifyContent={"center"}
+        marginTop={10}
+      >
+        <FormControlLabel
+          control={<Switch defaultChecked color="success" />}
+          label={
+            <Typography variant="h5" component="h5">
+              <LightbulbOutlinedIcon
+                sx={{ verticalAlign: "middle" }}
+                color="success"
+              />
+              Đèn
+            </Typography>
+          }
+        />
+        <FormControlLabel
+          control={<Switch color="warning" />}
+          label={
+            <Typography variant="h5" component="h5">
+              <FilterVintageOutlinedIcon
+                sx={{ verticalAlign: "middle" }}
+                color="warning"
+              />
+              Quạt
+            </Typography>
+          }
+        />
+      </Stack>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Server } from "socket.io";
 
 const app = express()
 
@@ -8,4 +9,44 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => console.log(`App listen on PORT: ${PORT}`))
+const server = app.listen(PORT, () => console.log(`App listen on PORT: ${PORT}`))
+
+
+// Socket
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+    },
+});
+
+let timeChange: ReturnType<typeof setInterval>;
+let count = 0;
+
+
+io.on("connection", (socket) => {
+    if (timeChange) clearInterval(timeChange);
+    console.log("connected to socket.io", socket.id);
+    timeChange = setInterval(() => {
+        count += 1;
+        const newData = {
+            label: count,
+            value: Math.floor(Math.random() * 100),
+        };
+        io.emit("dataUpdate", newData);
+    }, 1000)
+
+    // setInterval(() => {
+
+
+    //     // Emit data to all connected clients
+    //     io.emit('dataUpdate', newData);
+    // }, 1000);
+
+
+    socket.on("disconnect", () => {
+        console.log("socket disconnect");
+        // characters = characters.filter(
+        //     (value) => value.id !== socket.id)
+    });
+});
+
