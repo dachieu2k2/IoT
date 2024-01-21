8a5c3dce718a07f5e0a158d8e4d82fe050ca61ca
hihihaha <hieutedam2k2@gmail.com>
2024-01-21 20:31:19 +0700

    :fire: add animation background, switch, icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import WaterDropOutlinedIcon from "@mui/icons-material/WaterDropOutlined";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
 import FilterVintageOutlinedIcon from "@mui/icons-material/FilterVintageOutlined";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
 import { FormControlLabel, Switch, Typography } from "@mui/material";
 
 const s = io("http://localhost:4000/");
@@ -15,9 +16,19 @@ interface DataFromServer {
   labels: number[];
   values: number[];
 }
+interface StateData {
+  temperature: DataFromServer;
+  humidity: DataFromServer;
+  light: DataFromServer;
+}
 
 function App() {
   const [data, setData] = useState<DataFromServer>({ labels: [], values: [] });
+
+  const [checkedLight, setCheckedLight] = useState<boolean>(true);
+  const [checkedFan, setCheckedFan] = useState<boolean>(false);
+
+  // console.log(checkedLight, checkedFan);
 
   useEffect(() => {
     s.on("dataUpdate", (newData: any) => {
@@ -45,10 +56,27 @@ function App() {
   }, []);
 
   // console.log(data);
+  // console.log(data.values.reduce((p, c) => p + c, 0) / data.values.length);
 
   return (
-    <>
-      <Stack direction={"row"} spacing={2} marginTop={10}>
+    <div
+      style={{
+        height: "100vh",
+        width: "100%",
+        backgroundImage: `${
+          data.values.reduce((p, c) => p + c, 0) / data.values.length < 50
+            ? "linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%)"
+            : "linear-gradient(120deg, #f6d365 0%, #fda085 100%)"
+        }`,
+        transition: ` all 1s ease-in-out`,
+      }}
+    >
+      <Stack
+        direction={"row"}
+        spacing={2}
+        marginTop={10}
+        justifyContent={"center"}
+      >
         <Box justifyContent={"center"}>
           <Typography variant="h5" component="h5">
             <ThermostatOutlinedIcon
@@ -56,13 +84,21 @@ function App() {
               color={"error"}
             />
             Nhiệt độ
+            <Typography
+              variant="h5"
+              component="h5"
+              color={"red"}
+              marginLeft={3}
+            >
+              {data.values[data.values.length - 1]}
+            </Typography>
           </Typography>
           <LineChart
             xAxis={[
               {
                 scaleType: "point",
                 data: data.labels,
-                label: "Thời gian",
+                label: "Thời gian (s)",
               },
             ]}
             yAxis={[
@@ -71,7 +107,7 @@ function App() {
                 scaleType: "linear",
                 min: 0,
                 max: 120,
-                label: "Nhiệt độ",
+                label: "Nhiệt độ (°C)",
               },
             ]}
             series={[
@@ -94,13 +130,21 @@ function App() {
               color={"primary"}
             />
             Độ ẩm
+            <Typography
+              variant="h5"
+              component="h5"
+              color={"blue"}
+              marginLeft={3}
+            >
+              {data.values[data.values.length - 1]}
+            </Typography>
           </Typography>
           <LineChart
             xAxis={[
               {
                 scaleType: "point",
                 data: data.labels,
-                label: "Thời gian",
+                label: "Thời gian (s)",
               },
             ]}
             yAxis={[
@@ -108,8 +152,8 @@ function App() {
                 id: "a",
                 scaleType: "linear",
                 min: 0,
-                max: 120,
-                label: "Nhiệt độ",
+                max: 100,
+                label: "Phần trăm (%)",
               },
             ]}
             series={[
@@ -132,13 +176,21 @@ function App() {
               color="warning"
             />
             Ánh sáng
+            <Typography
+              variant="h5"
+              component="h5"
+              color={"#ff6d00"}
+              marginLeft={3}
+            >
+              {data.values[data.values.length - 1]}
+            </Typography>
           </Typography>
           <LineChart
             xAxis={[
               {
                 scaleType: "point",
                 data: data.labels,
-                label: "Thời gian",
+                label: "Thời gian (s)",
               },
             ]}
             yAxis={[
@@ -147,7 +199,7 @@ function App() {
                 scaleType: "linear",
                 min: 0,
                 max: 120,
-                label: "Nhiệt độ",
+                label: "Nhiệt độ (°C)",
               },
             ]}
             series={[
@@ -172,31 +224,85 @@ function App() {
         marginTop={10}
       >
         <FormControlLabel
-          control={<Switch defaultChecked color="success" />}
+          control={
+            <Switch
+              defaultChecked
+              color="success"
+              onChange={(event) => {
+                setCheckedLight(event.target.checked);
+              }}
+            />
+          }
           label={
             <Typography variant="h5" component="h5">
-              <LightbulbOutlinedIcon
-                sx={{ verticalAlign: "middle" }}
-                color="success"
-              />
+              {checkedLight ? (
+                <LightbulbIcon
+                  sx={{ verticalAlign: "middle" }}
+                  color="success"
+                  fontSize="large"
+                  style={{
+                    transition: ` all 1s ease-in-out`,
+                    animation: `${
+                      checkedLight && "neon"
+                    } 1.5s ease-in-out infinite alternate`,
+                  }}
+                />
+              ) : (
+                <LightbulbOutlinedIcon
+                  sx={{ verticalAlign: "middle" }}
+                  color="success"
+                  fontSize="large"
+                  style={{
+                    transition: ` all 1s ease-in-out`,
+                  }}
+                />
+              )}
               Đèn
             </Typography>
           }
         />
         <FormControlLabel
-          control={<Switch color="warning" />}
+          control={
+            <Switch
+              color="warning"
+              onChange={(event) => {
+                setCheckedFan(event.target.checked);
+              }}
+            />
+          }
           label={
             <Typography variant="h5" component="h5">
               <FilterVintageOutlinedIcon
                 sx={{ verticalAlign: "middle" }}
                 color="warning"
+                fontSize="large"
+                style={{
+                  animation: `${checkedFan && "spin"} 1s linear infinite`,
+                }}
               />
               Quạt
+              <style>{`
+            @keyframes spin {
+                 0% { transform: rotate(360deg); }
+                 100% { transform: rotate(0deg); }
+            }
+
+            @keyframes neon {
+              from {
+                filter: drop-shadow( 0 0 5px #fff) drop-shadow( 0 0 15px green) drop-shadow( 0 0 20px green);
+              }
+            
+              to {
+                
+                filter: drop-shadow( 0 0 20px #fff) drop-shadow( 0 0 25px green) drop-shadow( 0 0 40px green);
+              }
+            }
+              `}</style>
             </Typography>
           }
         />
       </Stack>
-    </>
+    </div>
   );
 }
 
